1788233e8bbe97c7f5ff1c8742d6595ef7bda900
agustinlopez1999 <lopezagustin1999@hotmail.com>
2025-06-06 00:59:22 -0300

    boton estilizado

--- scripts/modo-oscuro.js
+++ scripts/modo-oscuro.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
+    const $body = $('body');
+    const $boton = $('#modoOscuro');
+
     if (localStorage.getItem('modo') === 'oscuro') {
-        $('body').addClass('dark');
+        $body.addClass('dark');
+        $boton.html('☀️');
+    } else {
+        $boton.html('🌙');
     }
 
     $('#modoOscuro').click(function(){
         $('body').toggleClass('dark');
         if($('body').hasClass('dark')){
             localStorage.setItem('modo', 'oscuro');
+            $boton.html('☀️');
         }else{
             localStorage.setItem('modo','claro');
+            $boton.html('🌙');
         }
     });
 
